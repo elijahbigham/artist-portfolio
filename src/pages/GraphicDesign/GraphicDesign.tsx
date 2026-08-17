ebdcaf8b8b9ad9aedc592f7b56885b0c93a9d246
Elijah Bigham <elijahbigham@gmail.com>
2026-08-17 16:40:38 -0400

    graphic design images

---
+++ src/pages/GraphicDesign/GraphicDesign.tsx
@@ -0,0 +1,3 @@
+export default function GraphicDesignPage() {
+  return <h1>Graphic Design</h1>;
+}
---
+++ src/worksData.js
@@ -0,0 +1,9 @@
+const worksData = [
+  {
+    id: 1,
+    title: "Project 1",
+    description: "Description of Project 1",
+  },
+];
+
+export default worksData;

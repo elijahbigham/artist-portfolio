fbf3de5c9ed6e1afc0169c5fcfa1e59e5985682d
Elijah Bigham <elijahbigham@gmail.com>
2026-08-17 17:45:48 -0400

    start of data structure

--- src/worksData.js
+++ src/worksData.js
@@ -1,8 +1,58 @@
 const worksData = [
   {
-    id: 1,
-    title: "Project 1",
-    description: "Description of Project 1",
+    title: "Smoke Break",
+    tags: ["portfolio"],
+    imageFolder: "highway-vape",
+  },
+  {
+    title: "Rainbow Trout",
+    tags: ["portfolio"],
+    imageFolder: "flyrod-rainbow-trout",
+  },
+  {
+    title: "Street Scene",
+    tags: ["portfolio"],
+    imageFolder: "street scene",
+  },
+  {
+    title: "Millionaire Shortbread",
+    tags: ["portfolio"],
+    imageFolder: "millionaire-shortbread",
+  },
+  {
+    title: "Cat with Guitar",
+    tags: ["portfolio"],
+    imageFolder: "bolo-cat",
+  },
+  {
+    title: "Star Dress",
+    tags: ["portfolio"],
+    imageFolder: "star-dress",
+  },
+  {
+    title: "Western",
+    tags: ["portfolio"],
+    imageFolder: "western",
+  },
+  {
+    title: "Angel",
+    tags: ["portfolio"],
+    imageFolder: "angel",
+  },
+  {
+    title: "Astoria Park",
+    tags: ["portfolio"],
+    imageFolder: "astoria-park",
+  },
+  {
+    title: "All is Well Poster",
+    tags: ["graphic-design"],
+    imageFolder: "all-is-well-poster",
+  },
+  {
+    title: "All is Well Poster, Alternate Version",
+    tags: ["graphic-design"],
+    imageFolder: "all-is-well-poster-alt",
   },
 ];
 
